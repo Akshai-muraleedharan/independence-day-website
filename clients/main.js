1beb5b200241d1df5a49236a9978dd601151fba1
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-08-04 22:31:14 +0100

    index page half portion completed

--- clients/main.js
+++ clients/main.js
@@ -1,23 +1,14 @@
 
 let container =document.getElementById('container')
 
-const loader =document.querySelector('.loader')
-function displayLoading(){
-    
-     
-        loader.classList.add('display')  
-}
-
-// function displayLoaded(){
-//     loader.classList.remove('display') 
-// }
-
-
-
 function videoOutPut() {
     
     const dataOut = `
     <div class="video-container">
+         <div  id="qoutes-holder" style="position: absolute; height:100vh;" >
+            <p id="qoutes-ele"></p>
+            
+         </div>
         <video muted autoplay loop class="video-holder" id="video-ele" onload="loadFunction()">
             <source src="./assets/2024_08_04_19_36_08.mp4" >
         </video>
@@ -25,6 +16,22 @@ function videoOutPut() {
 
     container.innerHTML += dataOut
 }
+
+  
+  
+  
+
+    const loader =document.querySelector('.loader')
+    function displayLoading(){
+           
+            loader.classList.add('display')  
+    }
+
+    function displayLoaded(){
+        loader.classList.remove('display') 
+    }
+
+
 
 
 function loadFunction(){
@@ -41,6 +48,8 @@ function mainController(){
 
 mainController()
 let videoEle =document.querySelector('#video-ele');
+let quetes =document.querySelector('#qoutes-ele')
+
 
 
   const stopInterval = setInterval(function() {
@@ -50,10 +59,33 @@ let videoEle =document.querySelector('#video-ele');
      console.log(value);
         
         if(value > 5.66 ) {
-          console.log('hai');
-          clearInterval(stopInterval)
-          videoEle.pause()
-        }           
+          quetes.textContent = "1947 August 15 "
+          
+        }
+        
+        if(value > 10 ){
+            quetes.textContent = ""   
+        }
+
+        if(value > 14){
+            quetes.textContent ="Our Freedom start"
+        }
+
+        if(value > 17){
+            quetes.textContent =""
+        }
+
+        if(value > 20){
+            quetes.textContent ="At red fort"
+        }
+
+        if(value > 26){
+            quetes.textContent =""
+        }
+
+        if(value > 35){
+            window.location.href="./main.html"
+        }
     }
     , 1000);
    
